a560dd469f1cd23c703a8fadb7d77f7b01cee717
sahanpramudith <pramuditsahan466@gmail.com>
2024-04-05 04:51:11 +0530

    route

--- Addmin/src/main/main.jsx
+++ Addmin/src/main/main.jsx
@@ -22,6 +22,7 @@ import router from "../router/router.jsx";
 import {NavLink, Route, Routes} from "react-router-dom";
 import post from "../pages/createpost/post.jsx";
 import Post from "../pages/createpost/post.jsx";
+import Viwe from "../pages/Viwe/View.jsx";
 
 const drawerWidth = 240;
 
@@ -173,6 +174,7 @@ export default function Main() {
                 <Routes>
 
                     {getroute(router)}
+                    <Route path={'*'} element={<Viwe/>}></Route>
                 </Routes>
 
             </Box>
